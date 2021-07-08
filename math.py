be7ecf58d1b0010f12f44f716441ff2a83cd5559
shreyasnadig02 <shreyasnadig02@gmail.com>
2021-07-08 14:38:28 +0530

    Added fn headers for all fns

--- math.py
+++ math.py
@@ -1,8 +1,15 @@
+#Add implimentation
 def add(x,y):
     pass
+    
+#Subtract implimentation    
 def subtract(x,y):
     pass
+    
+#Multiply implimentation    
 def multiply(x,y):
     pass
+    
+#Divide implimentation    
 def divide(x,y):
     pass
